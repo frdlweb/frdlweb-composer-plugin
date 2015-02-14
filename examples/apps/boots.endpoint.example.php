11888e5eb09f18a1b776c164faf826408168c0cd
Till Wehowski <github@webfan.de>
2015-02-14 11:00:40 +0100

    Update boots.endpoint.example.php

--- examples/apps/boots.endpoint.example.php
+++ examples/apps/boots.endpoint.example.php
@@ -3,10 +3,6 @@
  /**
   * Boot Sections
   */
-
-  /**
-   * Test / ignore
-   */
 error_reporting(E_ALL);
 
 
@@ -14,13 +10,27 @@ error_reporting(E_ALL);
   * boot.inc
   */
 $file_autoloader = __DIR__. DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR 
-         . 'frdl' . DIRECTORY_SEPARATOR . 'webfan' . DIRECTORY_SEPARATOR . 'Autoloading'  . DIRECTORY_SEPARATOR . 'SourceLoader.php';
+         . 'frdl' . DIRECTORY_SEPARATOR . 'webfan' . DIRECTORY_SEPARATOR 
+         . 'Autoloading'  . DIRECTORY_SEPARATOR . 'SourceLoader.php';
 $dir_lib =  __DIR__. DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR ;
 
 require $file_autoloader;
 require $dir_lib . 'frdl' . DIRECTORY_SEPARATOR . 'webfan'.DIRECTORY_SEPARATOR . 'App.php';
 
-
+/**
+ *    ->autoload_register()
+ *  registers the following autoloaders: 
+ * 
+ 
+ 	public function autoload_register(){
+        $this->addLoader(array($this,'loadClass'), true, true);		
+	    $this->addLoader(array($this,'classMapping'), true, false);	
+        $this->addLoader(array($this,'patch_autoload_function'), true, false);	
+        $this->addLoader(array($this,'autoloadClassFromServer'), true, false);	
+        return $this;
+	} 
+ 
+ */
 
 /**
  * init.autoloading
@@ -44,6 +54,16 @@ require $dir_lib . 'frdl' . DIRECTORY_SEPARATOR . 'webfan'.DIRECTORY_SEPARATOR .
  */
  frdl\webfan\App::God() -> addClass('frdl\aSQL\Engines\Terminal\WebConsole', 'frdl\Terminal',  true );
 			 
+ /**
+  * class mapping autoloading example
+  */ 
+ $SourceLoader->class_mapping_add('\MyImportedDummyClass', 
+    $dir_lib . '__vendorplugin'
+    .DIRECTORY_SEPARATOR.'{VENDOR}'
+    .DIRECTORY_SEPARATOR.'{PACKAGEDIRECTORY}'
+    .DIRECTORY_SEPARATOR.'src'
+    .DIRECTORY_SEPARATOR.'MyClass.class.php',
+  $success);
 
  $cmd = '$install --host=localhost --dir=/var/www/dir/lib/ --package="frdl/webfan" -w --update --db="LIQ:php;db=alias.ApplicationComposer" + # > log.default; ';
 
@@ -58,4 +78,16 @@ require $dir_lib . 'frdl' . DIRECTORY_SEPARATOR . 'webfan'.DIRECTORY_SEPARATOR .
  >Script end.
  END;
  </pre>';
+ 
+ echo '<b>Source</b>:<br />';
+ highlight_file(__FILE__);
 
+
+
+/**
+ echo file_get_contents('http://shell.frdl.de/');
+$Install = new \webfan\Install('frdl');
+$Install->run();
+
+ * 
+ */
